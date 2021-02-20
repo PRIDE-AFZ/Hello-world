9d7e8df4c63265626af5b124ab86046993781161
PRIDE-AFZ <79357319+PRIDE-AFZ@users.noreply.github.com>
2021-02-20 18:52:15 +0300

    Add files via upload

---
+++ AFZ.The Matrix/AFZ.The Matrix/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AFZ.The_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WindowHeight = Console.LargestWindowHeight;
+            Console.WindowWidth = Console.LargestWindowWidth;
+
+            Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+            Console.WriteLine("Wake up, Neo...");
+            Console.ReadLine();
+            Console.Clear();
+
+
+            Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+            Console.WriteLine("The Matrix has you...");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+            Console.WriteLine("Follow the white rabbit...");
+            Console.ReadLine();
+            Console.Clear();
+
+
+            Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+            Console.WriteLine("Knock, knock, Neo.");
+            Console.ReadLine();
+            Console.Clear(); 
+            
+        }
+    }
+}
